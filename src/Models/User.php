a6fe8b1dce95c570a93c21e1b3885f7eeb71a9ef
vladar21 <vladar21@gmail.com>
2024-06-16 14:21:42 +0100

    add improve User model

--- src/Models/User.php
+++ src/Models/User.php
@@ -6,30 +6,37 @@ use App\Core\Database;
 use PDO;
 
 class User {
+    public $id;
+    public $username;
+    public $password;
+    public $role;
+
     /**
      * Find a user by username.
      *
      * @param string $username
-     * @return object|null
+     * @return User|null
      */
     public static function findByUsername($username) {
         $db = (new Database())->getConnection();
         $stmt = $db->prepare("SELECT * FROM users WHERE username = :username");
         $stmt->execute(['username' => $username]);
-        return $stmt->fetch(PDO::FETCH_OBJ);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, self::class);
+        return $stmt->fetch();
     }
 
     /**
      * Find a user by ID.
      *
      * @param int $id
-     * @return object|null
+     * @return User|null
      */
     public static function find($id) {
         $db = (new Database())->getConnection();
         $stmt = $db->prepare("SELECT * FROM users WHERE id = :id");
         $stmt->execute(['id' => $id]);
-        return $stmt->fetch(PDO::FETCH_OBJ);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, self::class);
+        return $stmt->fetch();
     }
 }
 ?>
